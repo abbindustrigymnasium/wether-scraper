f46aaeeeafe0fbbb70fc2a4096b8cb31bf4eace3
abb21lucbih <91330884+LucaBih@users.noreply.github.com>
2023-05-30 14:26:13 +0200

    deleted code

--- server/server.js
+++ server/server.js
@@ -62,10 +62,6 @@ app.post("/klart", async (req, res) => {
           }
           tempData[actualDate] = dayData;
         }
-
-
-
-
         fs.readFile("weather.json", "utf8", (err, jsonString) => {
           let weatherData = {};
           if (!err) {
@@ -84,10 +80,6 @@ app.post("/klart", async (req, res) => {
           });
         });
         await browser.close();
-
-
-
-
 })
 app.post("/smhi", async (req, res) => {
   const city = req.body.city;
@@ -182,84 +174,3 @@ app.post("/smhi", async (req, res) => {
   retryFetch();
 });
 
-// app.post("/smhi", async (req, res) => {
-//     const city = req.body.city
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto("https://www.smhi.se/", {
-//     waitUntil: "networkidle2",
-//     timeout: 120000,
-//   });
-//   await page.type(
-//     ".fullPage #root ._2iwYz ._3PmF5 ._2gHMK #downshift-0-input",
-//     city
-//   );
-//   await Promise.all([page.keyboard.press("Enter")]);
-//   await page.waitForSelector("._3JvZ9 li", { visible: true });
-//   const firstLink = await page.$("._3JvZ9 li");
-//   await firstLink.click();
-//   await page.waitForSelector("a.opWmC");
-
-//   const flerDagsKnapp = await page.$("a.opWmC");
-//   await flerDagsKnapp.click();
-//   await page.waitForNavigation();
-
-//   const ÖppnaAllaKnapp = await page.$("._2AvpL ._2qKDq .pUpND");
-//   if (ÖppnaAllaKnapp) {
-//     await ÖppnaAllaKnapp.click();
-//   }
-
-//   const buttons = await page.$$("button.xkPpi.oniiX._27-sD");
-//   const tables = await page.$$("div._2K-9O._3qBmj table");
-
-//   buttons.splice(-2);
-//   tables.splice(-2);
-
-//   const data = {}; // Object to store the data
-
-//   for (let i = 0; i < buttons.length; i++) {
-//     const button = buttons[i];
-//     const table = tables[i];
-//     const date = await button.$eval(
-//       "span.TUekU",
-//       (element) => element.textContent
-//     );
-//     const rows = await table.$$("tbody tr");
-//     const dayData = {};
-
-//     for (const row of rows) {
-//       const tid = await row.$eval("th", (element) => element.textContent);
-//       const time = tid + ":00";
-//       const temp = await row.$eval(
-//         "td .RXE5T span",
-//         (element) => element.textContent
-//       );
-//       dayData[time] = temp; // Store time and temperature in dayData object
-//     }
-
-//     data[date] = dayData; // Store dayData object in the data object
-
-
-//   }
-
-
-//   fs.readFile("weather.json", "utf8", (err, jsonString) => {
-//     let weatherData = {};
-//     if (!err) {
-//       weatherData = JSON.parse(jsonString);
-//     }
-//     weatherData.SMHI = { [city]: data };
-//     res.json(data)
-//     const json = JSON.stringify(weatherData, null, 2);
-
-//     fs.writeFile("weather.json", json, "utf8", (err) => {
-//       if (err) {
-//         console.error("Error writing JSON file:", err);
-//       } else {
-//         console.log("SMHI data saved to weather.json file.");
-//       }
-//     });
-//   });
-
-//   await browser.close();
-// })
